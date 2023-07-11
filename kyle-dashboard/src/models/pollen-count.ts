d95643b6a8e554e845eba3db41d6b280042188f6
kyle.malisos <kyle.malisos@hrblock.com>
2023-07-11 00:05:15 -0500

    Added API calls to recieve pollen count and risk for grass tree and weed and display results.

---
+++ kyle-dashboard/src/models/pollen-count.ts
@@ -0,0 +1,5 @@
+export interface PollenCount {
+    grass_pollen: number,
+    tree_pollen: number,
+    weed_pollen: number
+}
---
+++ kyle-dashboard/src/models/pollen-risk.ts
@@ -0,0 +1,5 @@
+export interface PollenRisk {
+    grass_pollen: string,
+    tree_pollen: string,
+    weed_pollen: string
+}
